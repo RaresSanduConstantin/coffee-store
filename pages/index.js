9b8b2cf917ec7d68a24b9b39f8638a94d473f770
Sandu <sandurares57@gmail.com>
2022-02-17 11:30:20 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Banner from '../components/banner';
+import Card from '../components/card';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const handleOnBtnClick = () => {
     console.log('Button clicked');
   };
+
+  const coffeStores = [1,2,3,4,5,6,7,8,9,10]
 
   return (
     <div className={styles.container}>
@@ -28,6 +31,23 @@ export default function Home() {
             width={700}
             height={400}
           />
+        </div>
+        <div className={styles.sectionWrapper}>
+          <h2 className={styles.heading2}>Stores near me</h2>
+          <div className={styles.cardLayout}>
+            {coffeStores.map((store) => {
+              return (
+
+                <Card
+                  key={store}
+                  name='Coffee Shop'
+                  imgUrl='/static/hero-image.png'
+                  href='coffee-store/darkhorse-coffeee'
+                  className={styles.card}
+                />
+              )
+            })}
+          </div>
         </div>
       </main>
     </div>
